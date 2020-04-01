9f7eb053f2f9f6392890d63b954cfe9ab4beb63e
Hava-hizzle <mikehavrilak@gmail.com>
2020-04-01 15:01:01 -0400

    require compression

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const comression = require("compression");
 
 
 const PORT = process.env.PORT || 3000;
